ad861eb5e0b99938416fae278ffd6e576fd6c1e9
EdC <edward.clayton@gmail.com>
2018-12-02 18:14:14 +0000

    Receive and send images from server

--- usersapi/model/user.js
+++ usersapi/model/user.js
@@ -1,7 +1,19 @@
 var mongoose = require('mongoose');
-const User = mongoose.model('User', new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: String,
-  email: String
-}));
+  email: String,
+  imageType: String,
+  image: {type: Buffer, select: false}
+});
+UserSchema.set('toJSON',{
+  transform: function(doc,ret,options){
+    var dto = {
+      email: ret.email,
+      name: ret.name,
+      _id: ret._id
+    }
+  }
+})
+const User = mongoose.model('User', UserSchema);
 
 module.exports = exports = User
--- usersapi/routes/users.js
+++ usersapi/routes/users.js
@@ -11,10 +11,22 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.post('/', function(req,res){
- // const data = Object.assign(req.body, { user: req.user.sub }) || {};
- 
-  User.create(req.body).then(user => {
+let setUserProperties = function(values){
+  var updated = {};
+  updated['email'] = values['email'];
+  updated['name'] = values['name'];
+  if (values['imageChanged'])
+  {
+    updated['image'] = new Buffer(values['image'], "base64");
+    updated['imageType'] = values['imageType'];
+  }
+  return updated;
+}
+
+router.post('/', function(req,res){ 
+  
+  var updated = setUserProperties(req.body);
+  User.create(updated).then(user => {
     res.json(user);
   })
   .catch(err => {
@@ -22,5 +34,39 @@ router.post('/', function(req,res){
     res.status(500).send(err);
   });
 });
+
+router.put('/:id', function(req,res){
+  var user_id = req.params.id;
+
+  var updated = setUserProperties(req.body);
+  
+
+  User.findByIdAndUpdate(user_id, updated, function(err, values) {
+    if (!err) {
+        res.json("okay");
+    } else {
+        res.write("fail");
+    }
+  });
+})
+
+router.get('/:id/avatar', function(req,res){
+  User.findById(req.params.id, function(err,user){
+    console.log(user.imageType);
+    console.log(user.name);
+    console.log(req.params.id)
+    if (user.image && user.imageType)
+    {
+      console.log('sending image of type ' + user.imageType);
+      res.contentType(user.imageType);
+      console.log(user.image);
+      res.end(user.image, "binary");
+    }
+    else{
+      res.statusCode=404;
+      res.send("");
+    }
+  }).select('image imageType');
+})
 
 module.exports = router;
